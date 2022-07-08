5e0a33043b1e260ff2edbe8adb32a241ae04ff00
Bruno Lisboa <97450047+BrunoLisboaS@users.noreply.github.com>
2022-07-08 11:01:10 -0300

    Add files via upload

---
+++ desafio1.ts
@@ -0,0 +1,13 @@
+
+interface employee {
+    code: number,
+    name: string
+}
+
+(() => {
+    const employeeJohn : employee = {
+        code: 10,
+        name: "John"
+    }
+    console.log(employeeJohn.name)
+})();
